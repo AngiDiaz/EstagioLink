0c744bb4bac2a42b1d41745dc3727e21a6f12c83
MarcosOliveC <20221910016@ifba.edu.br>
2024-12-27 17:59:20 -0300

    empresa atributos

--- PHP/criar-usuario.php
+++ PHP/criar-usuario.php
@@ -6,3 +6,16 @@
     header("Location: ../HTML/paginas-formulario/aluno/dados-pessoais.html");
     exit();
 ?>
+
+<?php
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $razao = $_POST['razao'];
+    $cnpj = $_POST['cnpj'];
+    $endereco = $_POST['endereco'];
+    $telefone = $_POST['telefone'];
+    $ramo = $_POST['ramo'];
+    $senha = $_POST['senha'];
+    header("Location: ../HTML/paginas-formulario/aluno/dados-pessoais.html");
+    exit();
+?>
