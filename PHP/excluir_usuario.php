34641e797754af6791a7675d26ddcd5ecddd3f3c
EstherLayssa <tetestrela0403@gmail.com>
2024-12-27 22:59:40 -0300

    Update excluir_usuario.php

--- PHP/excluir_usuario.php
+++ PHP/excluir_usuario.php
@@ -1,5 +1,5 @@
 <?php
-   include 'banco.php';
+   include 'conexao-banco.php';
    $id_usuario = filter_input(INPUT_GET,'id_usuario',FILTER_SANITIZE_NUMBER_INT);
    excluir_usuario($id_usuario);
    ?>
